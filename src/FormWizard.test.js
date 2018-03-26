8179c4b23c9cc1b5dfac0d77f65c59b7d362b46d
Knut Hühne <knut.huehne@correctiv.org>
2018-03-26 17:35:55 +0200

    make sure that OR conditions work

--- src/FormWizard.test.js
+++ src/FormWizard.test.js
@@ -48,38 +48,115 @@ const steps = [
   }
 ];
 
+const moreSteps = [
+  {
+    schema: {
+      title: "first",
+      properties: {
+        first_first: {
+          type: "boolean"
+        },
+        first_second: {
+          type: "string"
+        }
+      }
+    },
+    conditions: {}
+  },
+  {
+    schema: {
+      title: "third"
+    },
+    conditions: {
+      or: [
+        { first_first: { equal: true } },
+        { first_second: { equal: "banana" } }
+      ]
+    }
+  }
+];
+
 describe("FormWizard", () => {
   let instance;
 
-  beforeEach(() => {
-    const wrapper = shallow(
-      <FormWizard formData={{}} steps={steps} history={[]} investigation={{}} />
-    );
-    instance = wrapper.instance();
+  describe("easy conditions", () => {
+    beforeEach(() => {
+      const wrapper = shallow(
+        <FormWizard
+          formData={{}}
+          steps={steps}
+          history={[]}
+          investigation={{}}
+        />
+      );
+      instance = wrapper.instance();
+    });
+
+    it("should set first step", async () => {
+      expect(instance.state.schema.title).toBe("first");
+    });
+
+    it("should advance to correct state", async () => {
+      const nextStep = await instance.getNextStep({ first_first: false });
+      expect(nextStep.title).toBe("third");
+    });
+
+    it("should advance to another correct state", async () => {
+      const nextStep = await instance.getNextStep({ first_first: true });
+      expect(nextStep.title).toBe("second");
+    });
+
+    it("should reset formState if path changed", async () => {
+      instance.setState({
+        formData: { first_first: true, second_first: false }
+      });
+      const nextStep = await instance.getNextStep({ first_first: false });
+
+      expect(nextStep.title).toBe("third");
+
+      await instance.updateFormData({ first_first: false });
+      // note that the - now invalid - second_first is not in the data anymore
+      expect(instance.state.formData).toEqual({ first_first: false });
+    });
   });
 
-  it("should set first step", async () => {
-    expect(instance.state.schema.title).toBe("first");
-  });
+  describe("or conditions", () => {
+    beforeEach(() => {
+      const wrapper = shallow(
+        <FormWizard
+          formData={{}}
+          steps={moreSteps}
+          history={[]}
+          investigation={{}}
+        />
+      );
+      instance = wrapper.instance();
+    });
 
-  it("should advance to correct state", async () => {
-    const nextStep = await instance.getNextStep({ first_first: false });
-    expect(nextStep.title).toBe("third");
-  });
+    it("should work if the first condition is met", async () => {
+      const nextStep = await instance.getNextStep({ first_first: true });
+      expect(nextStep.title).toBe("third");
+    });
 
-  it("should advance to another correct state", async () => {
-    const nextStep = await instance.getNextStep({ first_first: true });
-    expect(nextStep.title).toBe("second");
-  });
+    it("should work if the second condition is met", async () => {
+      const nextStep = await instance.getNextStep({ first_second: "banana" });
+      expect(nextStep.title).toBe("third");
+    });
 
-  it("should reset formState if path changed", async () => {
-    instance.setState({ formData: { first_first: true, second_first: false } });
-    const nextStep = await instance.getNextStep({ first_first: false });
+    it("should not work if the conditions are not met", async () => {
+      const nextStep = await instance.getNextStep({
+        first_first: false,
+        first_second: "bread"
+      });
+      expect(nextStep).toBeUndefined();
+    });
 
-    expect(nextStep.title).toBe("third");
-
-    await instance.updateFormData({ first_first: false });
-    // note that the - now invalid - second_first is not in the data anymore
-    expect(instance.state.formData).toEqual({ first_first: false });
+    it("should not work if one of the conditions is met", async () => {
+      const nextStep = await instance.getNextStep({
+        first_first: false,
+        first_second: "banana"
+      });
+      expect(nextStep.title).toBe("third");
+    });
   });
 });

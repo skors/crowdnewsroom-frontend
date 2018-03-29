fd19365033c6f85be1e99d162bc62f23072ef52f
Knut Hühne <knut.huehne@correctiv.org>
2018-03-29 17:43:23 +0200

    style boolean buttons

--- src/ButtonWidget.js
+++ src/ButtonWidget.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import "./ButtonWidget.css";
+
 class ButtonWidget extends Component {
   constructor(props) {
     super(props);
@@ -14,34 +16,40 @@ class ButtonWidget extends Component {
     if (this.props.schema.type === "boolean") {
       return (
         <div>
-          <label
-            className={
-              "btn btn-secondary " + (this.props.value === true && "active")
-            }
-          >
-            {this.props.schema.enumNames[0]}
-            <input
-              className="sr-only"
-              type="radio"
-              name={this.props.id}
-              value="true"
-              onChange={this.set}
-            />
-          </label>
-          <label
-            className={
-              "btn btn-secondary " + (this.props.value === false && "active")
-            }
-          >
-            {this.props.schema.enumNames[1]}
-            <input
-              type="radio"
-              className="sr-only"
-              name={this.props.id}
-              value="false"
-              onChange={this.set}
-            />
-          </label>
+          <div>
+            <label
+              className={
+                "button-widget__button btn btn-secondary " +
+                (this.props.value === true && "active")
+              }
+            >
+              {this.props.schema.enumNames[0]}
+              <input
+                className="sr-only"
+                type="radio"
+                name={this.props.id}
+                value="true"
+                onChange={this.set}
+              />
+            </label>
+          </div>
+          <div>
+            <label
+              className={
+                "button-widget__button btn btn-secondary " +
+                (this.props.value === false && "active")
+              }
+            >
+              {this.props.schema.enumNames[1]}
+              <input
+                type="radio"
+                className="sr-only"
+                name={this.props.id}
+                value="false"
+                onChange={this.set}
+              />
+            </label>
+          </div>
         </div>
       );
     }

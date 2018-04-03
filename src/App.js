c587e9aa49631a795db84495876ab1924eeb43c6
Knut Hühne <knut.huehne@correctiv.org>
2018-04-03 11:40:15 +0200

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import StateHolder from "./StateHolder.js";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ThankYou from "./ThankYou";
 import fontawesome from "@fortawesome/fontawesome";
 import faSpinner from "@fortawesome/fontawesome-free-solid/faSpinner";

9acb0002ae607bf11ed97f956a27398736c8c381
Knut Hühne <knut.huehne@correctiv.org>
2018-04-03 17:42:01 +0200

    add German translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,33 @@
 import Polyglot from "node-polyglot";
 
+/* eslint-disable no-unused-vars*/
 const en = {
   form: {
     loading: "Loading...",
-    error: "There was an error loading the form data...",
-    verified_message:
-      "Your response has been verified by the editors already. If you want to make changes contact us at team@crowdnewsroom.org"
+    error: "There was an error loading the form data..."
   },
   summary: {
     message: "Please confirm your details:",
     edit_item: "Edit from here"
   }
 };
+/* eslint-enable */
 
-const polyglot = new Polyglot({ phrases: en });
+const de = {
+  form: {
+    loading: "Lädt...",
+    error: "Beim Laden des Formulars ist ein Fehler aufgetreten..."
+  },
+  summary: {
+    message: "Bitte bestätigen Sie Ihre Angaben:",
+    edit_item: "Von diesem Schritt aus bearbeiten"
+  }
+};
+
+// this would be the place where we decide if we pass the
+// `en` or the `de` object. Right now we just always pass
+// the German translations
+const polyglot = new Polyglot({ phrases: de });
 
 const t = key => polyglot.t(key);
 

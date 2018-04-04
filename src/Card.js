a16482adc0171b4086896c4071c8e91d9e90976c
Knut Hühne <knut.huehne@correctiv.org>
2018-04-04 11:37:45 +0200

    convert Card to functional component

--- src/Card.js
+++ src/Card.js
@@ -3,38 +3,33 @@ import PropTypes from "prop-types";
 
 import "./Card.css";
 
-class Card extends Component {
-  static propTypes = {
-    logo: PropTypes.string,
-    title: PropTypes.string,
-    dataPrivacyUrl: PropTypes.string
-  };
-
-  render() {
-    return (
-      <div className="card">
-        <img
-          className="card__logo"
-          src={this.props.logo}
-          alt={this.props.title}
-        />
-        <div className="card__body">{this.props.children}</div>
-        <div className="card__footer">
-          {this.props.dataPrivacyUrl ? (
-            <a
-              className="card__footer--data-privacy"
-              href={this.props.dataPrivacyUrl}
-              target="_blank"
-              rel="noopener"
-            >
-              Datenschutz
-            </a>
-          ) : (
-            ""
-          )}
-        </div>
+function Card(props) {
+  return (
+    <div className="card">
+      <img className="card__logo" src={props.logo} alt={props.title} />
+      <div className="card__body">{props.children}</div>
+      <div className="card__footer">
+        {props.dataPrivacyUrl ? (
+          <a
+            className="card__footer--data-privacy"
+            href={props.dataPrivacyUrl}
+            target="_blank"
+            rel="noopener"
+          >
+            Datenschutz
+          </a>
+        ) : (
+          ""
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Card.propTypes = {
+  logo: PropTypes.string,
+  title: PropTypes.string,
+  dataPrivacyUrl: PropTypes.string
+};
+
 export default Card;

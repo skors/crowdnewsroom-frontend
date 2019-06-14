1ca8d911e51d8a58e26c01ea6ef9d63c68ef2cde
Ricardo Lafuente <r@manufacturaindependente.org>
2019-06-14 17:46:30 +0100

    WIP to get image uploads working

--- src/formWidgets/ImageUploadWidget.js
+++ src/formWidgets/ImageUploadWidget.js
@@ -1,6 +1,68 @@
 import React from "react";
 import FileWidget from "react-jsonschema-form";
 
+class ImageUploadWidget extends FileWidget {
+  encodedValue = "";
+
+  constructor(props) {
+    super(props);
+    // init the value property, example had this so I just copied it over
+    this.state = { value: "" };
+    this.set = this.set.bind(this);
+  }
+
+  set(event) {
+    // get the file data
+    var files = event.target.files;
+    var reader = new FileReader();
+    reader.readAsBinaryString(files[0]);
+    // we can't use 'this' to refer to the widget inside the onload callback below
+    var widget = this;
+    reader.onload = function() {
+      // now convert to base64 and save it
+      // the hidden field is bound to state.value so will update itself
+      widget.setState({ value: btoa(reader.result) });
+    };
+    reader.onerror = function() {
+      console.log("Error using FileReader to get base64 image data.");
+    };
+  }
+
+  render() {
+    const { multiple, id, readonly, disabled, autofocus } = this.props;
+    // const { filesInfo } = this.state;
+    return (
+      <div>
+        <p>
+          <input
+            ref={ref => (this.inputRef = ref)}
+            id={id + "-wrapper"}
+            type="file"
+            disabled={readonly || disabled}
+            onChange={this.set}
+            autoFocus={autofocus}
+            multiple={multiple}
+            accept="image/*"
+            capture="environment"
+          />
+          <input
+            ref={ref => (this.inputRef = ref)}
+            id={id}
+            name={this.props.id}
+            type="hidden"
+            value={this.state.value}
+          />
+        </p>
+      </div>
+    );
+  }
+}
+/*
+        <FilesInfo filesInfo={filesInfo} />
+        */
+
+export default ImageUploadWidget;
+
 /*
 function FilesInfo(props) {
   const { filesInfo } = props;
@@ -21,33 +83,3 @@ function FilesInfo(props) {
   );
 }
 */
-
-class ImageUploadWidget extends FileWidget {
-  render() {
-    const { multiple, id, readonly, disabled, autofocus } = this.props;
-    // const { filesInfo } = this.state;
-    return (
-      <div>
-        <p>
-          <input
-            ref={ref => (this.inputRef = ref)}
-            id={id}
-            type="file"
-            disabled={readonly || disabled}
-            onChange={this.onChange}
-            defaultValue=""
-            autoFocus={autofocus}
-            multiple={multiple}
-            accept="image/*"
-            capture="environment"
-          />
-        </p>
-      </div>
-    );
-  }
-}
-/*
-        <FilesInfo filesInfo={filesInfo} />
-        */
-
-export default ImageUploadWidget;

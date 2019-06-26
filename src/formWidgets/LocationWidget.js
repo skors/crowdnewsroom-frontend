6c1c1c569f3e49ddf8098bd98aae97b317cbbf49
Ricardo Lafuente <r@manufacturaindependente.org>
2019-06-26 16:36:01 +0100

    Finish working location widget

--- src/formWidgets/LocationWidget.js
+++ src/formWidgets/LocationWidget.js
@@ -5,52 +5,42 @@ import "./LocationWidget.css";
 class LocationWidget extends Component {
   constructor(props) {
     super(props);
-    //this.set = this.set.bind(this);
     this.state = { latlon: "" };
+    this.buttonText = this.props.schema.title;
   }
-
-  /*
-  set(event) {
-    this.props.onChange(event.target.value === "true");
-  }
-  */
 
   onClick = event => {
     event.preventDefault();
     const widget = this;
+    widget.buttonText = "Getting your location...";
     navigator.geolocation.getCurrentPosition(
       function(position) {
-        widget.setState({
-          latlon: position.coords.latitude + "," + position.coords.longitude
-        });
+        var value = position.coords.latitude + "," + position.coords.longitude;
+        widget.setState({ latlon: value });
+        widget.buttonText = "Location received!";
+        return widget.props.onChange(value === "" ? "" : value);
       },
       function(error) {
         console.log(error);
+        widget.buttonText = "Error fetching location! Click to try again.";
       }
     );
   };
 
   render() {
-    console.log("latlon: " + this.state.latlon);
     return (
       <div className="location">
-        <button
-          className="button"
-          ref={ref => (this.inputRef = ref)}
-          name={this.props.id}
-          onClick={this.onClick}
-        >
-          <i className="fi-marker" />
-          <span>{this.props.schema.title}</span>
-        </button>
         <input
           ref={ref => (this.inputRef = ref)}
           name={this.props.id}
-          value={this.state.latlon}
-          type="text"
+          id={this.props.id}
+          type="hidden"
           readOnly={true}
-          // disabled={readonly || disabled}
         />
+        <button className="button" onClick={this.onClick}>
+          <i className="fi-marker" />
+          <span>{this.buttonText}</span>
+        </button>
       </div>
     );
   }

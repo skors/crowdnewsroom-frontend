9b7d139de8d8665e7370b47bda04a7448b069f30
Danny Wagner <wagner@expositio.de>
2020-07-08 15:20:32 +0200

    adding name to input

--- src/formWidgets/PatternTypeTextInputWidget.js
+++ src/formWidgets/PatternTypeTextInputWidget.js
@@ -17,6 +17,7 @@ class PatternTypeTextInputWidget extends Component {
         placeholder={props.placeholder}
         className="form-control"
         value={own_this.state.value}
+        name={this.props.id}
         onChange={e => {
           own_this.setState({ value: e.target.value });
         }}

ddc11f206a8d32c6def3da59065704744eaabbac
Danny Wagner <wagner@expositio.de>
2020-07-08 14:57:40 +0200

    Adding dynmic value changing for own widget

--- src/formWidgets/PatternTypeTextInputWidget.js
+++ src/formWidgets/PatternTypeTextInputWidget.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 
 class PatternTypeTextInputWidget extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
   render() {
     const props = this.props;
+    const own_this = this;
+
     return (
       <input
         placeholder={props.placeholder}
         className="form-control"
-        value={props.value}
+        value={own_this.state.value}
+        onChange={e => {
+          own_this.setState({ value: e.target.value });
+        }}
         id={props.id}
         pattern={props.schema.pattern}
         type={props.schema.field_type ? props.schema.field_type : "text"}
